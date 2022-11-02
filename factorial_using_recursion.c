8de589154070e10d85a4baf0e01a7788d7b41ffc
darkknight6767 <78910295+darkknight6767@users.noreply.github.com>
2022-11-02 14:46:00 +0530

    Create factorial_using_recursion.c

---
+++ factorial_using_recursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int factorial(num)
+{
+  if(num==0) return 1;
+  return num*factorial(num);
+}
+
+int main()
+{
+int num;
+printf("Enter number: ");
+scanf("%d",&num);
+printf("the factorial of %d is %d",num,factorial(num));
+}
